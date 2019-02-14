550a1223a8bf7133155ee1bb91fe7df5cbe4eead
tscloud <thomas.s.cloud@gmail.com>
2019-02-14 16:30:06 -0500

    Parse additional weather data out of MQTT data in Clock

--- ESP8266/ESP8266NTPClock/mqtt.h
+++ ESP8266/ESP8266NTPClock/mqtt.h
@@ -61,24 +61,34 @@ const char *getTemp() {
   // T63.64,H37.72,P1035.76
   static char buffer[20];
   String tempString = "";
+  String humString = "";
   String presString = "";
 
   if (weatherData.length() != 0)
   {
     Serial.println(s+"weather data: "+weatherData);
+    // positions of each data type
     int tempLoc = weatherData.indexOf("T");
     int humLoc = weatherData.indexOf("H");
     int presLoc = weatherData.indexOf("P");
+    // parse out each data type
     tempString = weatherData.substring(tempLoc+1, humLoc-1);
+    humString = weatherData.substring(hunLoc+1, presLoc-1);
     presString = weatherData.substring(presLoc+1, weatherData.length());
+    // format temp
     float tempFloat = round(tempString.toFloat());
     tempString = String((int)tempFloat);
-    tempString += char(247);
+    tempString += char(247);// degree symbol
+    // format humidity
+    float humFloat = round(humString.toFloat());
+    humString = String((int)humFloat);
+    humString += char(247);// percent symbol <- find/make
   }
   else {
     tempString = "--";
   }
   Serial.println(s+"temp data: "+tempString);
+  Serial.println(s+"hum data: "+humString);
   Serial.println(s+"pres data: "+presString);
   tempString.toCharArray(buffer, tempString.length()+1);
   return buffer;
